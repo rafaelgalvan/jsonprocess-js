bf6816c7ccc2951d5955307aae3dfce5e03260de
Rafael Galvan <rafaelgalvan@outlook.com>
2024-05-08 10:59:09 -0300

    adição de endpoints, adicionada documentação nos métodos e criando novo método para pegar o endpoint do objeto

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const config =  require('./config/config.json');
 const endpoints = require('./config/endpoints.json');
 const atividade = require('./data/atividade.json');
-const { 
+const {
+    get_object_endpoint,
     send_post_request, 
     send_put_request, 
     send_delete_request,
@@ -11,6 +12,7 @@ const {
 // verifica se o parâmetro commit foi enviado na chamada
 const commit = process.argv[2];
 
+// identificador do conjunto de objetos que fazem parte da atividade
 const codigoDaObra = atividade.idAtividade;
 
 function getHeaders() {
@@ -20,47 +22,67 @@ function getHeaders() {
     });
 }
 
+/**
+ * Envia o commit da atividade
+ */
 async function enviarCommit() {
-    let url = config.url + 'obra/commit'
+    let url = config.url + 'obra/commit';
     let objeto = {"codigoDaObra": codigoDaObra};
     console.log(`Enviando Commit da obra ${codigoDaObra}`);
     let response = await send_commit_request(url, getHeaders(), objeto);
     console.log(`Commit enviado. Resposta: ${response.status}: ${response.statusText}`);
 }
 
+/**
+ * Envia para o servidor os objetos que serao inseridos
+ * @param {Array} objetos Array com os objetos
+ */
 async function inserirObjetos(objetos) {
     console.log("::: Atividade - id: " + codigoDaObra);
     for (const objeto of objetos) {
         objeto.codigoDaObra = codigoDaObra;
-        let url = config.url + endpoints[objeto.type];
+        let endpoint = get_object_endpoint(objeto, endpoints);
+        let url = config.url + endpoint;
 
         let response = await send_post_request(url, getHeaders(), objeto);
         console.log(`Objeto: ${objeto.type}, ID: ${objeto.id}, resposta: ${response.status}: ${response.statusText}`);
     }
 }
 
+/**
+ * Envia para o servidor os objetos que serao alterados
+ * @param {Array} objetos Array com os objetos
+ */
 async function alterarObjetos(objetos) {
     for (const objeto of objetos) {
         objeto.codigoDaObra = codigoDaObra;
-        let url = `${config.url}${endpoints[objeto.type]}/${objeto.id}`
+        let endpoint = get_object_endpoint(objeto, endpoints);
+        let url = `${config.url}${endpoint}/${objeto.id}`
 
         let response = await send_put_request(url, getHeaders(), objeto);
         console.log(`Objeto: ${objeto.type}, ID: ${objeto.id}, resposta: ${response.status}: ${response.statusText}`);
     }
 }
 
+/**
+ * Envia para o servidor os objetos que serao removidos
+ * @param {Array} objetos Array com os objetos
+ */
 async function removerObjetos(objetos) {
     let headers = getHeaders();
     headers.delete('Content-Type');
 
     for (const objeto of objetos) {
         objeto.codigoDaObra = codigoDaObra;
-        let url = `${config.url}${endpoints[objeto.type]}/${codigoDaObra}/${objeto.id}`;
+        let endpoint = get_object_endpoint(objeto, endpoints);
+        let url = `${config.url}${endpoint}/${codigoDaObra}/${objeto.id}`;
         let response = await send_delete_request(url, headers);
         console.log(`Objeto: ${objeto.type}, ID: ${objeto.id}, resposta: ${response.status}: ${response.statusText}`);
     }
 }
-
+/**
+ * Processamento dos objetos
+ */
 async function processarObjetos() {
     if (atividade.entidadesCadastradas !== undefined && atividade.entidadesCadastradas.length > 0) {
         console.log("::: Represando objetos inseridos :::")
